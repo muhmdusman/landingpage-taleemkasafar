e82c8d44640a3b6719c36686e3f2e60cc9c4bf84
muhmdusman <amusman9705@gmail.com>
2026-09-26 15:40:41 +0500

    Clean repo contents

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://taleemkasafar.com', // replace with your actual domain
-    generateRobotsTxt: true,           // generates robots.txt file
-    changefreq: 'daily',
-    priority: 0.9,
-    sitemapSize: 5000,
-  }
-  
